b7cea874de01ac396be4c7d29ded5b66d97d2b52
seo8257 <barrac7@gmail.com>
2026-06-12 09:37:32 +0900

    Update
    Update

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+import os
+import tempfile
+
+import streamlit as st
+from langchain_community.document_loaders import CSVLoader
+from langchain_text_splitters import (    RecursiveCharacterTextSplitter )
+from langchain_chroma import (    Chroma  )
+from langchain_openai import   OpenAIEmbeddings,    ChatOpenAI 
+from langchain_classic.chains import (   create_retrieval_chain )
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+
+from langchain_core.prompts import (   ChatPromptTemplate )
+from langchain_core.callbacks import   BaseCallbackHandler
+
+st.title("📄 CSV File Reader")
+st.write("----------------")
