8269dbcf04b536adb94b651cdbee00d693aefc38
seo8257 <barrac7@gmail.com>
2026-06-12 09:50:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ from langchain_core.callbacks import   BaseCallbackHandler
 
 st.title("📄 CSV File Reader")
 st.write("----------------")
+
+openai_key = st.text_input(  "OPENAI_API_KEY",    type="password" )
+
+uploaded_file = st.file_uploader(   "CSV 파일을 올려주세요",   type=["csv"] )
+st.write("----------------")
